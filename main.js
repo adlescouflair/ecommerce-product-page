2661944d881f3221731c4ecbf5b28a480404ca54
Alain Lescouflair <adlescouflair@gmail.com>
2023-05-07 02:26:08 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var primary_nav = document.querySelector('.primary_navigation');
+var nav_toggle =  document.querySelector('.nav_toggle');
+var lightbox = document.querySelector('.lightbox');
+
+nav_toggle.addEventListener('click', ()=>{
+
+   var isvisible = JSON.parse(primary_nav.getAttribute('data-visible'));
+   if( isvisible== false){
+    primary_nav.setAttribute('data-visible', true)
+    nav_toggle.setAttribute('aria-expanded', true);
+    lightbox.setAttribute('data-visible', true);
+   }
+   else{
+    primary_nav.setAttribute('data-visible', false)
+    nav_toggle.setAttribute('aria-expanded', false);
+    lightbox.setAttribute('data-visible', false);
+   }
+    
+});
